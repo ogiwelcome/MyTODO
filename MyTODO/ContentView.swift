42b38bbaa8f12869be17b50c85431efa4f8d4c3c
ogiwelcome <ogi.fordevelopment@gmail.com>
2021-09-20 23:37:03 +0900

    add login form

--- MyTODO/ContentView.swift
+++ MyTODO/ContentView.swift
@@ -9,15 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Text("Hello, world!")
-                .padding()
-        }
+        LoginView()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
